aa77a9286189ace77dad7e85087c7e7b690bb8d3
salvagd09 <salva6770@gmail.com>
2026-02-10 20:08:52 -0500

    End of the lab

--- expressBookReviews/final_project/index.js
+++ expressBookReviews/final_project/index.js
@@ -4,6 +4,7 @@ const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 const app = express();
+let books = require("./router/booksdb.js");
 app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 app.use("/customer/auth/*", function auth(req,res,next){
@@ -22,9 +23,32 @@ app.use("/customer/auth/*", function auth(req,res,next){
         return res.status(403).json({message:"El usuario no ha iniciado sesion."})
     }
 });
- 
 const PORT =5000;
-
+/*Functionts that allows the using of axios */
+// Get book details based on ISBN
+genl_routes.get('/books/isbn/:isbn',function (req, res) {
+    const isbn=parseInt(req.params.isbn)
+    let librosIsbn=books[isbn]
+    if(!librosIsbn){
+      return res.status(404).json({message:"No existe libro con este ISBN"})
+    }
+    return res.status(200).json(librosIsbn)})
+//Get book details based on title
+genl_routes.get('/books/title/:title',function (req, res) {
+    const titulo=req.params.title
+    let claves=Object.keys(books)
+    const librosPorTitulo=[]
+    claves.forEach((clave)=>{
+      if(books[clave].title===titulo){
+          librosPorTitulo.push(books[clave])
+      }
+    })
+    if(librosPorTitulo.length>0){
+      return res.status(200).json(librosPorTitulo)
+    }else{
+      return res.status(404).json({message:`There is no book with title ${titulo}`})
+    }
+  })
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
--- expressBookReviews/final_project/router/general.js
+++ expressBookReviews/final_project/router/general.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const express=require('express');
+const axios= require('axios')
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -45,50 +46,53 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn=parseInt(req.params.isbn)
-  let librosIsbn=books[isbn]
-  if(!librosIsbn){
-    return res.status(404).json({message:"No existe libro con este ISBN"})
-  }
-  return res.status(200).send(JSON.stringify(librosIsbn,null,4));
- });
-  
+ public_users.get('/isbn/:isbn',async(req,res)=>{
+    const isbn=parseInt(req.params.isbn)
+    try{
+        const response=await axios.get(`https://salva6770-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/books/isbn/${isbn}`)
+        return res.status(200).json(response.data)
+    }catch(error){
+        return res.status(404).json({message:"Book wasn't found"})
+    }
+ })
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   const autor=req.params.author
   const librosAutor=[]
   let claves=Object.keys(books)
+  //Use Promise
   claves.forEach((clave)=>{
     if(books[clave].author===autor){
         librosAutor.push(books[clave])
     }
   })
-  if(librosAutor.length>0){
-    return res.status(200).json(librosAutor);
-  }else{
-    return res.status(400).json({message:`There is no book written by ${autor} in this list`})
-  }
+  let PromiseAuthor=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        if(librosAutor.length>0){
+            resolve(librosAutor)
+        }else{
+            reject(`There is no book written by ${autor} in this list`)
+        }
+    },3000)
+  })
+  PromiseAuthor.then((LibrosAutorColocado)=>{
+    return res.status(200).json(LibrosAutorColocado)}).catch(error=>{
+        return res.status(404).json({message:error})
+    })
   }
 );
-
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title',async(req, res)=>{
   const titulo=req.params.title
-  let claves=Object.keys(books)
-  const librosPorTitulo=[]
-  claves.forEach((clave)=>{
-    if(books[clave].title===titulo){
-        librosPorTitulo.push(books[clave])
-    }
-  })
-  if(librosPorTitulo.length>0){
-    return res.status(200).json(librosPorTitulo)
-  }else{
+  try{
+    const response=await axios
+    .get
+    (`https://salva6770-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/books/title/${titulo}`)
+    return res.status(200).json(response.data)
+  }catch(error){
     return res.status(404).json({message:`There is no book with title ${titulo}`})
   }
 });
-
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   let isbn=req.params.isbn
